bf638aa851c4d7c05dc482c0734430d5b9837f2e
thevks <techikida@gmail.com>
2022-07-21 09:10:10 +0530

    Update Q5-string-vallid-shuffle.cpp

--- strings/Q5-string-vallid-shuffle.cpp
+++ strings/Q5-string-vallid-shuffle.cpp
@@ -1,1 +1,14 @@
 //Write a program to check if a string is valid suffle of two strings or not
+
+string s1 = "XY";
+string s2 = "12";
+
+string s = "1XY2";   //Valid shuffle
+string s = "Y1X2";   //Valid shuffle
+string s = "Y21XX";  //Not a valid shuffle
+
+
+
+
+
+
